e4360b024cafc1dc4349f1a06737f0fbf54326c1
Filip <filipkk96@gmail.com>
2025-04-26 19:43:46 +0200

    Import resources

---
+++ imports.tf
@@ -0,0 +1,21 @@
+/*
+import {
+  to = azurerm_resource_group.main
+  id = "/subscriptions/81d677b5-e197-4b2e-b9fc-254b49d7a794/resourceGroups/RG1"
+}
+
+import {
+  to = azurerm_virtual_network.main
+  id = "/subscriptions/81d677b5-e197-4b2e-b9fc-254b49d7a794/resourceGroups/RG1/providers/Microsoft.Network/virtualNetworks/vnet1"
+}
+
+import {
+  to = azurerm_subnet.subnet1
+  id = "/subscriptions/81d677b5-e197-4b2e-b9fc-254b49d7a794/resourceGroups/RG1/providers/Microsoft.Network/virtualNetworks/vnet1/subnets/subnet1"
+}
+
+import {
+  to = azurerm_subnet.subnet2
+  id = "/subscriptions/81d677b5-e197-4b2e-b9fc-254b49d7a794/resourceGroups/RG1/providers/Microsoft.Network/virtualNetworks/vnet1/subnets/subnet2"
+}
+*/
---
+++ providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.27.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "81d677b5-e197-4b2e-b9fc-254b49d7a794"
+  features {
+  }
+}
